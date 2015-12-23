dc6771530a9ac825a1187cb9466db7476a4375e0
Shawn Sandy <shawnsandy04@gmail.com>
2015-12-23 17:32:26 -0500

    installed bower packages

--- gulp/tasks/browser-sync.js
+++ gulp/tasks/browser-sync.js
@@ -2,31 +2,30 @@
 
 var config = require('../config.json').browserSync,
     gulp = require('gulp'),
-  notify = require('gulp-notify'),
+    notify = require('gulp-notify'),
     browserSync = require('browser-sync');
-
 
 
 gulp.task('browser-sync', function () {
 
     browserSync({
-      server: {
-        baseDir: config.baseDir
-    },
-    files: [config.files],
-    timestamp: config.timestamp
+        server: {
+            baseDir: config.baseDir
+        },
+        files: [config.files],
+        timestamp: config.timestamp
     })
 
-})
+});
 
-gulp.task('browser-sync:notify',function(){
+gulp.task('browser-sync:notify', function () {
 
-  gulp.src(config.baseDir + '/index.html')
-  .pipe(notify('BROWSER-SYNC : Reloading browser(s)'));
-  browserSync.reload();
+    gulp.src(config.baseDir + '/index.html')
+        .pipe(notify('BROWSER-SYNC : Reloading browser(s)'));
+    browserSync.reload();
 
-})
+});
 
 gulp.task('browser-sync:reload', function () {
-  browserSync.reload();
+    browserSync.reload();
 });
